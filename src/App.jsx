1c172b0f7f55dea90b3c4662f6a94830067b5aa1
vavasoftdev01 <165743409+vavasoftdev01@users.noreply.github.com>
2025-05-22 11:28:52 +0800

    widget-integration
    - react tradingview widgets
    - minimal ui-fixes

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,34 @@ const HistoricalTradesLeaderBoardPanel = lazy(() =>
   import('./components/HistoricalTradesLeaderBoardPanel')
 );
 
+import { SingleTicker, TickerTape, AdvancedRealTimeChart, Ticker, SymbolInfo   } from "react-ts-tradingview-widgets";
+
+
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
+
+  const [symbols, setSymbols] = useState([
+    {
+      "proName": "BINANCE:BTCUSDT",
+      "title": "BTCUSDT"
+    },
+    {
+      "proName": "BINANCE:ETHUSDT",
+      "title": "ETHUSDT"
+    },
+    {
+      "proName": "BINANCE:BNBUSDT",
+      "title": "BNBUSDT"
+    },
+    {
+      "proName": "BINANCE:SOLUSDT",
+      "title": "SOLUSDT"
+    },
+    {
+      "proName": "BINANCE:DOGEUSDT",
+      "title": "DOGEUSDT"
+    }
+  ]);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -20,9 +46,10 @@ const App = () => {
 
   return (
     <div className="h-auto w-full bg-gray-900 p-6 text-gray-100 font-sans drop-shadow-xl/50">
-      <h1 className="text-2xl font-bold text-cyan-400 mb-4 hover:text-cyan-300 transition-colors duration-300">
-        {/* KBCGAME Trading Dashboard */}
-      </h1>
+      {/* <h1 className="text-2xl font-bold text-cyan-400 mb-4 hover:text-cyan-300 transition-colors duration-300">
+        KBCGAME Trading Dashboard
+      </h1> */}
+      
       {isLoading ? (
         <div className="flex justify-center items-center h-[400px]">
           <div className="loading loading-dots w-20 text-purple-600"></div>
@@ -36,9 +63,29 @@ const App = () => {
           }
         >
           <div className="flex flex-col gap-1 max-h-[100%] max-w-[1680px] mx-auto">
+            <div className="w-full">
+              <div className="tradingview-widget-container text-red-300">
+                {/* <SingleTicker
+                  colorTheme="dark"
+                  width="100%"
+                  height={10}
+                  symbol="BINANCE:BTCUSDT"
+                  locale="en"
+                  isTransparent={false}
+                /> */}
+
+                <Ticker locale="kr" symbols={symbols} colorTheme="dark"></Ticker>
+
+                {/* <SymbolInfo colorTheme="dark" symbol="BINANCE:BTCUSDT" autosize></SymbolInfo> */}
+
+                {/* <TickerTape colorTheme="dark" symbols={symbols}></TickerTape> */}
+              </div>
+              
+            </div>
             <div className="flex flex-row gap-1">
               <div className="w-2/3 flex flex-col border border-solid border-slate-700 rounded-tl-lg rounded-bl-lg h-full">
                 <BBBaselineChart height={500} />
+                {/* <AdvancedRealTimeChart theme="dark" height={500} style="2" interval="1" symbol='btcusdt'></AdvancedRealTimeChart> */}
                 <div className="w-full p-5 bg-slate-900 h-1/4">
                   <BettingForm />
                 </div>
@@ -50,6 +97,9 @@ const App = () => {
             <div className="bg-slate-900 border border-solid border-slate-700 rounded-b-lg p-6 flex justify-between overflow-auto min-h-[400px]">
               <HistoricalTradesLeaderBoardPanel />
             </div>
+            <footer>
+              {/* <Ticker locale="kr" symbols={symbols} colorTheme="dark"></Ticker> */}
+            </footer>
           </div>
         </Suspense>
       )}
--- src/components/BettingForm.jsx
+++ src/components/BettingForm.jsx
@@ -19,15 +19,36 @@ function BettingForm() {
         <div className="flex flex-col text-slate-400 font-medium">
           <span className="p-1 text-shadow-lg">How it works?</span>
           <div className="btn-multi w-full flex flex-row gap-1">
-            <button className="btn btn-primary">$5</button>
-            <button className="btn btn-primary">$10</button>
-            <button className="btn btn-primary">$15</button>
-            <button className="btn btn-primary">$25</button>
-            <button className="btn btn-primary">$50</button>
+            <button className="btn bg-gradient-to-b from-red-500 to-orange-500 hover:underline hover:shadow-md shadow-orange-500/50">$5</button>
+            <button className="btn bg-gradient-to-b from-red-500 to-orange-500 hover:underline hover:shadow-md shadow-orange-500/50">$10</button>
+            <button className="btn bg-gradient-to-b from-red-500 to-orange-500 hover:underline hover:shadow-md shadow-orange-500/50">$15</button>
+            <button className="btn bg-gradient-to-b from-red-500 to-orange-500 hover:underline hover:shadow-md shadow-orange-500/50">$25</button>
+            <button className="btn bg-gradient-to-b from-red-500 to-orange-500 hover:underline hover:shadow-md shadow-orange-500/50">$50</button>
           </div>
         </div>
       </div>
       <div className="flex flex-row w-full gap-1">
+      <button
+          className="group btn btn-success w-1/2 hover:text-white flex flex-row items-center justify-center gap-2"
+          onClick={() => handleClick('UP')}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1em"
+            height="1em"
+            fill="none"
+            viewBox="0 0 38 38"
+            className="detrade-icon size-10 rounded-full p-1.5 text-white pointer-events-none bg-up rotate-180 group-hover:rotate-[-525deg] group-hover:duration-500"
+          >
+            <path
+              fill="currentColor"
+              fillOpacity="0.75"
+              d="m33.429 14.206-6.464 6.464-5.373-5.375-5.999 5.998L19 24.7H9.5v-9.5l3.407 3.407 8.685-8.685 5.373 5.373 4.702-4.7a15.2 15.2 0 1 0 1.762 3.61zm2.922-2.96.019.02-.008.007A18.9 18.9 0 0 1 38 19c0 10.494-8.506 19-19 19S0 29.494 0 19 8.506 0 19 0c7.733 0 14.383 4.617 17.35 11.246"
+            />
+          </svg>
+          <span className="text-white">High</span>
+        </button>
+
         <button
           className="group btn btn-secondary w-1/2 hover:text-white flex flex-row items-center justify-center gap-2"
           onClick={() => handleClick('DOWN')}
@@ -46,28 +67,7 @@ function BettingForm() {
               d="m33.429 14.206-6.464 6.464-5.373-5.375-5.999 5.998L19 24.7H9.5v-9.5l3.407 3.407 8.685-8.685 5.373 5.373 4.702-4.7a15.2 15.2 0 1 0 1.762 3.61zm2.922-2.96.019.02-.008.007A18.9 18.9 0 0 1 38 19c0 10.494-8.506 19-19 19S0 29.494 0 19 8.506 0 19 0c7.733 0 14.383 4.617 17.35 11.246"
             />
           </svg>
-          <span className="text-white">Down</span>
-        </button>
-
-        <button
-          className="group btn btn-success w-1/2 hover:text-white flex flex-row items-center justify-center gap-2"
-          onClick={() => handleClick('UP')}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
-            fill="none"
-            viewBox="0 0 38 38"
-            className="detrade-icon size-10 rounded-full p-1.5 text-white pointer-events-none bg-up rotate-180 group-hover:rotate-[-525deg] group-hover:duration-500"
-          >
-            <path
-              fill="currentColor"
-              fillOpacity="0.75"
-              d="m33.429 14.206-6.464 6.464-5.373-5.375-5.999 5.998L19 24.7H9.5v-9.5l3.407 3.407 8.685-8.685 5.373 5.373 4.702-4.7a15.2 15.2 0 1 0 1.762 3.61zm2.922-2.96.019.02-.008.007A18.9 18.9 0 0 1 38 19c0 10.494-8.506 19-19 19S0 29.494 0 19 8.506 0 19 0c7.733 0 14.383 4.617 17.35 11.246"
-            />
-          </svg>
-          <span className="text-white">Up</span>
+          <span className="text-white">Low</span>
         </button>
       </div>
     </div>
